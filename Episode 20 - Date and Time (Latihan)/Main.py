b77f1c4b7a228e8bff7f4f54222e21a06f2689a6
Faqihza Mukhlish <faqihza.m@gmail.com>
2021-08-14 10:17:26 +0700

    update episode 20

---
+++ Episode 20 - Date and Time (Latihan)/Main.py
@@ -0,0 +1,20 @@
+# Date and time (latihan)
+
+import datetime as dt
+
+print("Silahkan masukan tanggal, \nbulan dan tahun lahir anda \n")
+tanggal = int(input("Tanggal \t: "))
+bulan = int(input("Bulan \t\t: "))
+tahun = int(input("Tahun \t\t: "))
+
+tanggal_lahir = dt.date(tahun,bulan,tanggal)
+print(f"Tanggal lahir anda adalah : {tanggal_lahir}")
+
+hari_ini = dt.date.today()
+print(f"Hari ini tanggal: {hari_ini}")
+umur_hari = hari_ini - tanggal_lahir
+umur_tahun = umur_hari.days // 365
+umur_bulan_sisa = (umur_hari.days % 365) // 30
+
+print(f"Hari nya adalah : {tanggal_lahir:%A}")
+print(f"Umur anda adalah: {umur_tahun} tahun, {umur_bulan_sisa} bulan")
