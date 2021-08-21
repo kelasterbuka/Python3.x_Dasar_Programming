3eea0a4d054fcc22886f6127218e872321bc11e0
Faqihza Mukhlish <faqihza.m@gmail.com>
2021-08-21 22:42:33 +0700

    update

---
+++ Episode 21 - If dan Else Statement/Main.py
@@ -0,0 +1,31 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="ucup" : print("Kamu Ganteng abieeez!!!!")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+# if nama=="ucup":
+# 	print("kamu ganteng abieeeez!")
+# 	print("kamu juga keren banget!")
+# print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama=="otong":
+	print("hai otooong, si keren!!!")
+else:
+	print("Ah kamu bukan otong, kamu gak keren! :(")
+
+print("akhir dari program")
