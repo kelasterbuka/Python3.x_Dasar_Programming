3e8aebb9fd20a4909e97b3910336ff74b080047d
Faqihza Mukhlish <faqihza.m@gmail.com>
2021-09-12 09:10:05 +0700

    update

---
+++ Episode 24 - For Loop (Perulangan)/Main.py
@@ -0,0 +1,46 @@
+# Perulangan (loop)
+
+# for kondisi:
+# 	aksi
+
+# ini dengan list
+angka2_list = [0,2,4,8,10] # ini adalah list
+print(angka2_list)
+
+for i in angka2_list:
+	print(f"i sekarang -> {i}")
+
+print("akhir dari program 1 \n")
+
+# ini dengan range
+angka2_range = range(5)
+
+for i in angka2_range:
+	print(f"i sekarang -> {i}")
+
+print("akhir dari program 2 \n")
+
+angka2_range = range(1,10)
+
+for i in angka2_range:
+	print(f"i sekarang -> {i}")
+	# print("saya keren")
+
+print("akhir dari program 3 \n")
+
+# menggunakan string
+
+data_str = "saya ganteng abiees"
+
+for huruf in data_str:
+	print(huruf)
+
+print("akhir dari program 4 \n")
+
+
+
+
+
+
+
+
---
+++ Episode 25 - While Loop (Perulangan)/Main.py
@@ -0,0 +1,17 @@
+# while loop
+
+# while kondisi:
+# 	aksi ini
+# 	aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+	angka += 1
+	print(f"angka sekarang -> {angka}")
+	print("otong ganteng maxsyimaal!")
+
+print("cukuuup")
