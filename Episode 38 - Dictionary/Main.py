5cb4a2adfce22918b769a5a608753b18b950306e
Faqihza Mukhlish <faqihza.m@gmail.com>
2022-03-30 15:42:53 +0700

    update

---
+++ Episode 38 - Dictionary/Main.py
@@ -0,0 +1,22 @@
+# list -> array, mengakses dengan 
+# menggunakan index
+
+data_list = ['ucup','otong','dudung']
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+	'key':'value',
+	'cp':'ucup',
+	'tg':'otong',
+	'dg':'dudung',
+	'nmbr':100,
+	'list':data_list,
+}
+
+print(data_dict['tg'])
+print(data_dict['nmbr'])
+print(data_dict['list'])
